c11c7f17b494d867a05c4b2625802830a08bb333
Hikari7 <h.kobe712@gmail.com>
2023-02-17 13:06:36 -0800

    added folders

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,6 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivateRoutes from "./PrivateRoutes";
 
 function App() {
   return (
